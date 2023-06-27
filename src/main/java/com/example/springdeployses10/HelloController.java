97b1580d253f79f37c4dbb2030d7d412d5dc59d2
JurBej <ajurbej2707@g.educaand.es>
2023-06-27 20:56:09 +0200

    ajustar url pagina principal

--- src/main/java/com/example/springdeployses10/HelloController.java
+++ src/main/java/com/example/springdeployses10/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
         System.out.println(message);
         return "Hola Mundo crueeel que feo eres";
     }
-    @GetMapping("/bootstrap")
+    @GetMapping("/")
     public String bootstrao(){
         return """
                 <!doctype html>
